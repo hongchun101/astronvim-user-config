9ef875aa1a78eae2d5573b80d8aa7cad41397e51
hongchun <1251341510@qq.com>
2023-06-29 16:49:10 +0800

    添加HL映射

--- mappings.lua
+++ mappings.lua
@@ -20,6 +20,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["H"] = { "^" },
+    ["L"] = { "$" },
   },
   t = {
     -- setting a mapping to false will disable it
