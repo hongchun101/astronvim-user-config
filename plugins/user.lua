82840b8c83bc547b0e47c4b20318b162ca5fb5d7
hongchun <1251341510@qq.com>
2023-07-06 10:14:18 +0800

    add plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,11 +9,4 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  {
-    "ggandor/leap.nvim",
-    event = "BufRead",
-    config = function()
-      require("leap").add_default_mappings()
-    end
-  },
 }
