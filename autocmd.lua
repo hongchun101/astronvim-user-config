ff7fb79067bb0c4d6e89281c3515972a558472bf
hongchun <1251341510@qq.com>
2023-07-21 22:04:58 +0800

    add neovide config

---
+++ autocmd.lua
@@ -0,0 +1,6 @@
+local utils = require "astronvim.utils"
+
+if vim.g.neovide then
+  local neovide = require "user.utils.neovide"
+  neovide.init()
+end
---
+++ plugins/community-register.lua
@@ -0,0 +1,3 @@
+return {
+  { import = "astrocommunity.register.nvim-neoclip-lua" },
+}
