d523a872b5df126bad977d093776ecb147b557ff
hongchun <1251341510@qq.com>
2023-08-27 19:11:13 +0800

    add plugins

---
+++ plugins/neoai.lua
@@ -0,0 +1,20 @@
+return {
+  "Bryley/neoai.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = {
+    "NeoAI",
+    "NeoAIOpen",
+    "NeoAIClose",
+    "NeoAIToggle",
+    "NeoAIContext",
+    "NeoAIContextOpen",
+    "NeoAIContextClose",
+    "NeoAIInject",
+    "NeoAIInjectCode",
+    "NeoAIInjectContext",
+    "NeoAIInjectContextCode",
+  },
+  opts = {},
+}
